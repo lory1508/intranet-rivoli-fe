b4251d3469cc49b480b0bb0586ac99d13c89cbb4
Lorenzo <lorenzo.galassi91@gmail.com>
2025-05-27 14:54:27 +0200

    feat: link utili

--- components/home/UsefulLinks.vue
+++ components/home/UsefulLinks.vue
@@ -2,11 +2,11 @@
   <div
     class="flex flex-col gap-2 p-4 rounded-lg shadow-md bg-secondaryLight text-darkAccent border-secondaryLight shadow-zinc-300"
   >
-    <div class="flex flex-row gap-2 pb-2">
-      <Icon :icon="icon" height="32" />
-      <div class="text-2xl">{{ title }}</div>
+    <div v-if="icon || title" class="flex flex-row gap-2 pb-2">
+      <Icon v-if="icon" :icon="icon" height="32" />
+      <div v-if="title" class="text-2xl">{{ title }}</div>
     </div>
-    <div class="grid grid-cols-2 gap-2 xl:grid-cols-1 2xl:grid-cols-2">
+    <div class="grid gap-2" :class="monoColumn ? 'grid-cols-1 w-fit' : 'grid-cols-2 xl:grid-cols-1 2xl:grid-cols-2'">
       <a
         v-for="link in links"
         :key="link.slug"
@@ -15,7 +15,10 @@
         class="flex flex-row items-center w-full font-semibold text-white transition-all duration-300 rounded-xl hover:ring-2 hover:shadow-md"
         :class="getCustomColors(link.slugType).ring"
       >
-        <div class="flex items-center w-full h-10 gap-2 pl-4 rounded-l-xl" :class="getCustomColors(link.slugType).bg">
+        <div
+          class="flex items-center w-full h-10 gap-2 pl-4 pr-4 rounded-l-xl"
+          :class="getCustomColors(link.slugType).bg"
+        >
           {{ link.title }}
         </div>
         <div class="flex items-center h-10 px-2 rounded-r-xl" :class="getCustomColors(link.slugType).iconBg">
@@ -42,6 +45,10 @@
     links: {
       type: Array,
       default: () => [],
+    },
+    monoColumn: {
+      type: Boolean,
+      default: false,
     },
   })
 
--- pages/link-utili.vue
+++ pages/link-utili.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    
+    <LoaderComponent v-if="loading" />
+    <div v-else>
+      <TitleComponent title="Link utili" :breadcrumb="breadcrumb" />
+
+      <!-- Link utili generali -->
+      <UsefulLinks :links="usefulLinks" class="w-full" mono-column />
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+  import TitleComponent from '~/components/common/TitleComponent.vue'
+  import LoaderComponent from '~/components/common/LoaderComponent.vue'
 
+  import UsefulLinks from '~/components/home/UsefulLinks.vue'
+  import { getExternalLinks } from '~/api/externalLinks'
+
+  const loading = ref(false)
+  const usefulLinks = ref([])
+
+  const breadcrumb = ref([
+    {
+      title: 'Home',
+      slug: '/',
+    },
+    {
+      title: 'Link Utili',
+      slug: '/link-utili',
+    },
+  ])
+
+  onMounted(async () => {
+    try {
+      loading.value = true
+      usefulLinks.value = await getExternalLinks()
+    } catch (error) {
+      console.error(error)
+    } finally {
+      loading.value = false
+    }
+  })
 </script>
-
-<style>
-
-</style>
