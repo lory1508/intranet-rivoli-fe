4e981399fc1f6b9028f96cc53eacc84b448ac5fe
Lorenzo Galassi <lorenzo.galassi91@gmail.com>
2025-10-06 09:11:13 +0200

    feat: updated color balance in home

---
+++ components/common/CardTitle.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="icon || title" class="flex flex-row items-center justify-between text-neutralDark">
+    <div class="flex flex-row gap-2">
+      <Icon v-if="icon" :icon="icon" height="32" />
+      <div v-if="title" class="text-xl font-semibold">{{ title }}</div>
+    </div>
+    <Button
+      v-if="buttonTitle && buttonDestination"
+      :title="buttonTitle"
+      color="darkGray"
+      icon="solar:arrow-right-linear"
+      width="w-fit"
+      icon-placement="right"
+      @clicked="$router.push(buttonDestination)"
+    />
+  </div>
+</template>
+
+<script setup>
+  import { Icon } from '@iconify/vue'
+  import Button from '~/components/common/Button.vue'
+
+  const props = defineProps({
+    icon: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    buttonTitle: {
+      type: String,
+      default: '',
+    },
+    buttonDestination: {
+      type: String,
+      default: '',
+    },
+  })
+</script>
