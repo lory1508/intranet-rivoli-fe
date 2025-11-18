ab342181b3469b3e33533098d5ba985939d9dade
Lorenzo Galassi <lorenzo.galassi91@gmail.com>
2025-11-18 11:07:51 +0100

    fix: set static target

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ import Components from "unplugin-vue-components/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   ssr: false,
+  target: "static",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
 
